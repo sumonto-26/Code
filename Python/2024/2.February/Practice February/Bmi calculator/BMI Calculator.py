5a0e9da141d1689a18d5cda0b44f801026dd92ca
sumonto-26 <sumontodatta26@gmail.com>
2024-02-17 17:58:11 +0600

    update code

--- Python/2024/2.February/Practice February/Bmi calculator/BMI Calculator.py
+++ Python/2024/2.February/Practice February/Bmi calculator/BMI Calculator.py
@@ -3,32 +3,34 @@ from tkinter import ttk
 
 def calculate_bmi():
     try:
-        width_value = float(width_entry.get())
+        weight_value = float(weight_entry.get())
         height_value = float(height_entry.get())
 
-        if width_string.get() == "pound" and height_string.get() == "inches":
-            ans = 703 * (width_value / (height_value ** 2))
-        elif width_string.get() == "pound" and height_string.get() == "meter":
-            ans = 703 * (width_value / ((height_value * 39.3701) ** 2))
-        elif width_string.get() == "pound" and height_string.get() == "centimeter":
-            ans = 703 * (width_value / ((height_value * 0.393701) ** 2))
+        if weight_string.get() == "pound" and height_string.get() == "inches":
+            ans = 703 * (weight_value / (height_value ** 2))
+        elif weight_string.get() == "pound" and height_string.get() == "meter":
+            ans = 703 * (weight_value / ((height_value * 39.3701) ** 2))
+        elif weight_string.get() == "pound" and height_string.get() == "centimeter":
+            ans = 703 * (weight_value / ((height_value * 0.393701) ** 2))
 
-        elif width_string.get() == "kilogram" and height_string.get() == "inches":
-            ans = width_value / ((height_value * 0.0254) ** 2)
-        elif width_string.get() == "kilogram" and height_string.get() == "meter":
-            ans = width_value / (height_value ** 2)
-        elif width_string.get() == "kilogram" and height_string.get() == "centimeter":
-            ans = width_value / ((height_value * 0.01) ** 2)
+        elif weight_string.get() == "kilogram" and height_string.get() == "inches":
+            ans = weight_value / ((height_value * 0.0254) ** 2)
+        elif weight_string.get() == "kilogram" and height_string.get() == "meter":
+            ans = weight_value / (height_value ** 2)
+        elif weight_string.get() == "kilogram" and height_string.get() == "centimeter":
+            ans = weight_value / ((height_value * 0.01) ** 2)
 
         bmi_result.set(f"Your BMI is: {ans:.2f}")
+        result_label['background'] = "lightgreen"
 
-    except ValueError:
+    except:
         bmi_result.set("Please enter valid numeric values")
+        result_label['background'] = "red"
 
 root = tk.Tk()
-root.geometry("600x550")
+root.geometry("800x600")
 root.title("BMI Calculator")
-root.resizable(False, False)
+root.minsize(800 , 600)
 root.configure(bg = "grey15")
 
 label1 = tk.Label(
@@ -50,7 +52,7 @@ label2.place(x = 10, y = 20)
 
 label3 = tk.Label(
     master = root,
-    text = "Width",
+    text = "Weight",
     background = "lightblue",
     foreground = "grey13",
     font = "None 15 bold"
@@ -71,23 +73,23 @@ height_types = [
     "meter",
     "centimeter"
 ]
-width_types = [
+weight_types = [
     "kilogram",
     "pound"
 ]
 
-width_string = tk.StringVar()
-width_com = ttk.Combobox(master = root, width = 10, textvariable = width_string, font = "none 15 bold")
-width_com["values"] = width_types
-width_com.place(x = 400, y = 170)
+weight_string = tk.StringVar()
+weight_com = ttk.Combobox(master = root, width = 10, textvariable = weight_string, font = "none 15 bold")
+weight_com["values"] = weight_types
+weight_com.place(x = 400, y = 170)
 
 height_string = tk.StringVar()
 height_com = ttk.Combobox(master = root, width = 10, textvariable = height_string, font = "none 15 bold")
 height_com.configure(values = height_types)
 height_com.place(x = 400, y = 225)
 
-width_entry = ttk.Entry(master = root, font = "none 12 bold")
-width_entry.place(x = 175, y = 172)
+weight_entry = ttk.Entry(master = root, font = "none 12 bold")
+weight_entry.place(x = 175, y = 172)
 
 height_entry = ttk.Entry(master = root, font= "none 12 bold")
 height_entry.place(x = 175, y = 227)
@@ -96,7 +98,7 @@ bmi_result = tk.StringVar()
 result_label = tk.Label(
     master = root,
     textvariable = bmi_result,
-    background = "lightblue",
+    background = "lightgreen",
     foreground = "grey13",
     font = "None 20 bold"
 )
