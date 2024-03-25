b526720a55e1e416b559f3f02c7ec09fb1f11998
sumonto-26 <sumontodatta26@gmail.com>
2024-03-25 20:27:27 +0600

    add some code

--- C++/2024/3. March/Beecrowd/Untitled-2.cpp
+++ C++/2024/3. March/Beecrowd/Untitled-2.cpp
@@ -185,7 +185,6 @@ int main(){
     return 0;
 }
 
-*/
 
 #include <iostream>
 using namespace std;
@@ -204,6 +203,54 @@ int main(){
     else{
         cout << "Valores nao aceitos" << endl ;
     }
+
+    return 0;
+}
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function declaration
+std::string print_ans(const int arr[], int n) {
+    // Example: Concatenate all elements of the array into a string
+    std::string result;
+    for (int i = 0; i < n; ++i) {
+        result += std::to_string(arr[i]) + "\n";
+    }
+    return result;
+
+
+}
+int main(){
+
+    int n = 3;
+    int* arr = new int[n];
+    for (int i = 0;  i<n; i++){
+        cin >> arr[i];
+    }
+
+    string ans = print_ans(arr, n);
+    bubbleSort(arr, n);
+    cout << print_ans(arr, n);
+    cout << endl;
+    cout << ans;    
+
 
     return 0;
 }
