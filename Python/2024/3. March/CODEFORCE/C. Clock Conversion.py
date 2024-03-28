34dc38b5f083241123b17e52385674c92ca17d30
sumonto-26 <sumontodatta26@gmail.com>
2024-03-28 23:13:34 +0600

    add some code

--- Python/2024/3. March/CODEFORCE/C. Clock Conversion.py
+++ Python/2024/3. March/CODEFORCE/C. Clock Conversion.py
@@ -1,3 +1,4 @@
+#my logic
 for _ in range(int(input())):
     a = input()
     hour, minute = map(int, a.split(':'))
@@ -9,3 +10,16 @@ for _ in range(int(input())):
         print(f"{hour:02d}:{minute:02d} PM")
     else:
         print(f"{hour-12:02d}:{minute:02d} PM")
+
+#tourist's logic
+'''import sys
+
+for _ in range(int(input())):
+    h, m = map(int, input().split(':'))
+    s = " AM"
+    if h >= 12:
+        h -= 12
+        s = " PM"
+    if h == 0:
+        h = 12
+    print(f"{h:02d}:{m:02d}{s}")'''
