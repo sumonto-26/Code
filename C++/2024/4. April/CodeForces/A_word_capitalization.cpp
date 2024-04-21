ad7997dd6209d87379d43f3d5ba409376839a2d9
sumonto-26 <sumontodatta26@gmail.com>
2024-04-21 11:51:03 +0600

    add some code

---
+++ C++/2024/4. April/CodeForces/A_word_capitalization.cpp
@@ -0,0 +1,13 @@
+//DATE ==> 21 April 2024
+// Author ==> SUMONTO
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s;
+
+    return 0;
+}
