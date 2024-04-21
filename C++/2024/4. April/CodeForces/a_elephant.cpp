7cf82663d2c1f7184425fb4c049e7b2762165ea1
sumonto-26 <sumontodatta26@gmail.com>
2024-04-21 13:24:48 +0600

    add some code

---
+++ C++/2024/4. April/CodeForces/a_elephant.cpp
@@ -0,0 +1,20 @@
+//DATE ==> 21 April 2024
+//Autor ==> SUMONTO
+
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans = 0;
+
+    while (n>0){
+        n -= 5;
+        ans ++;
+
+    }
+    cout << ans;
+
+    return 0;
+}
