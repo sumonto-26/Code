a409ad97329b8c52e946c7d146e0b5440ee73b32
sumonto-26 <sumontodatta26@gmail.com>
2024-04-26 21:07:36 +0600

    add some code

---
+++ C++/2024/4. April/CodeForces/a_insomnia_cure.cpp
@@ -0,0 +1,25 @@
+// DATE ==> 26 April 2024
+// AUTHOR ==> SUMONTO
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+    
+    if (k == 1 || l == 1 || m == 1 || n == 1) {
+        cout << d << endl;
+    } else {
+        int ans = 0;
+        for (int i = 1; i <= d; ++i) {
+            if (i % k != 0 && i % l != 0 && i % m != 0 && i % n != 0) {
+                ans ++;
+            }
+        }
+        cout << d-ans << endl;
+    }
+
+    return 0;
+}
