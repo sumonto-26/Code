21b40b2ace491e0d0f21f008e664fc876619b2a1
sumonto-26 <sumontodatta26@gmail.com>
2024-04-28 14:06:58 +0600

    add some code

---
+++ C++/2024/4. April/practice April/practice_2.cpp
@@ -0,0 +1,38 @@
+// Topic Name ==> Blanced bracket.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string is_balanced(string s){
+    unordered_map <char,int> symbol_list = {{'(',-1}, {'{',-2}, {'[',-3},{')',1}, {'}',2}, {']',3}};
+    stack <char> st;
+    for (auto &value : s){
+        if(symbol_list[value] < 0){
+            st.push(value);
+        }
+        else{
+            if (st.empty() || symbol_list[st.top()] + symbol_list[value] != 0){
+                return "NO";
+            }
+            st.pop();
+        }
+    }
+    if (st.empty()){
+        return "YES";
+    }
+    else{
+        return "NO";
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    while(--n){
+        string s;
+        cin >> s;
+        cout << is_balanced(s) << endl;
+    }
+
+    return 0;
+}
