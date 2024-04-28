7734abf9ff14562fde09e2dccf2de3882a3b420a
sumonto-26 <sumontodatta26@gmail.com>
2024-04-29 00:31:30 +0600

    add some code

--- C++/2024/4. April/practice April/STL/practice_stl_12.cpp
+++ C++/2024/4. April/practice April/STL/practice_stl_12.cpp
@@ -5,17 +5,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> NGE(vector<int> v){
+    vector <int> nge(v.size());
+    stack <int> st;
+    for (int i=0; i<v.size(); ++i){
+        while(!st.empty() && v[i] >v[st.top()]){
+            nge[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty()){
+        nge[st.top()] = -1;
+        st.pop();
+    }
+    return nge;
+}
+
 int main(){
 
     int n;
     cin >> n;
-    vector <int> vec;
+    vector <int> vec(n);
     for (int i=0; i<n; ++i){
         cin >> vec[i];
     }
 
-    for (auto value : vec){
-        cout << value << ' ';
+    vector<int> nge = NGE(vec);
+    for (int i=0; i<n; ++i){
+        // cout << vec[i] << " " << (nge[i] == -1 ? -1 : vec[nge[i]]) <<endl;
+        cout << (nge[i] == -1 ? -1 : vec[nge[i]]) << " ";
     }
 
     return 0;
