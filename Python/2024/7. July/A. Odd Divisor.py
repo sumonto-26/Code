62e931cd95b51558a8bca6e0a38afbd8dafbbf0d
sumonto-26 <sumontodatta26@gmail.com>
2024-07-25 20:07:26 +0600

    Add Some Code

--- Python/2024/7. July/A. Odd Divisor.py
+++ Python/2024/7. July/A. Odd Divisor.py
@@ -1,5 +1,16 @@
 # DATE ==> 25 JULY 2024
 # AUTHOR ==> SUMONTO
+
+# Method ==> 1
 for _ in range(int(input())):
     n = int(input())
     print("YES" if n&(n-1) != 0 else "NO")
+
+# Method ==> 2
+for _ in range(int(input())):
+    n = int(input())
+    if (n%2 != 0): print("YES")
+    else:
+        while n > 1:
+            n = n/2
+        print("YES" if n!=1 else "NO")
