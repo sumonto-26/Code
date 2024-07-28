e2e8c822d0fb8ad68360e22a2532154560b5c9f6
sumonto-26 <sumontodatta26@gmail.com>
2024-07-28 21:50:51 +0600

    Add Some Code

---
+++ C , C++/2024/7. July/Coder Army DSA playlist/lecture_7.cpp
@@ -0,0 +1,87 @@
+// DATE ==> 28 JULY 2024
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+/*
+    int x;
+    cout << "Enter a Number between 1-50 : ";
+    cin >> x;
+
+    for(int row=1; row <= x; row++){
+        for(int col=1; col <= row; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+*/
+
+/*
+    for(int row = 1; row <= 5; row++){
+        for(int col=1; col <= row; col++){
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+*/
+
+/*
+    for(int row = 1; row <= 5; row++){
+        for(int col = 1; col <= row; col++){
+            cout << row << " ";
+        }
+        cout << endl;
+    }
+*/
+
+/*
+    for(int row=1; row <=5; row++){
+        for(int col = row; col >= 1; col--){
+            cout << col << ' ';
+        }
+        cout << endl;
+    }
+*/
+
+/*
+    for (char row = 1; row <= 26; row ++){
+        char ch = 'a'+(row-1);
+        for(int col = 1; col<=row; col++){
+            cout << ch << "  "; 
+        }
+        cout << endl;
+    }
+*/
+
+/*
+    for (int row = 1; row <= 5; row ++){
+        for (int col = row; col <= 5; col ++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+*/
+
+/*
+    for (int row = 1; row <= 5; row++){
+        for (int col = 1; col <= 6-row; col++){
+            cout << col << ' ';
+        }
+        cout << endl; 
+    }
+*/
+
+    for(int row = 1; row <= 5; row++){
+        for(int col = 5; col >= 6-row; col--){
+            cout << col << ' ';
+        }
+        cout << endl;
+    }
+
+
+
+
+    return 0;
+};
