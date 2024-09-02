e0c3d9a7a8b2a36276d68d67171e1c6f43bb5232
sumonto-26 <sumontodatta26@gmail.com>
2024-09-02 13:48:29 +0600

    Add Some Code

---
+++ C , C++/2024/9. September/Codeforces/contests/Codeforces_Round_955.cpp
@@ -0,0 +1,20 @@
+// DATE ==> 2 SEPTEMBER 2024
+// Contest Name ==> Codeforces Round 955 (Div. 2, with prizes from NEAR!)
+
+// Problem A ==> A. Soccer
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // int n; cin >> n;
+    // while(n--){
+    //     int x1,y1,x2,y2;
+    //     cin >> x1 >> y1 >> x2 >> y2 ;
+    //     if (x1==x2 && y1==y2) cout << "YES"<< endl;
+    //     else if(x1<y1 && x2>y2 || x1>y1 && x2<y2) cout << "NO"<< endl;
+    //     else cout << "YES" << endl;
+    // }
+}
