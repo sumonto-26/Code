37d0e41d334ea473afff3ef6a9341baaf913250a
sumonto-26 <sumontodatta26@gmail.com>
2025-04-05 19:05:29 +0600

    Add some code

---
+++ Python/2025/4. Aprill 2025/Competitive programming/Codeforces/A. Candy Bags.py
@@ -0,0 +1,5 @@
+n = int(input())
+n2 = (n*n)//2
+for i in range(1,n2+1):
+    print(i, (n*n+1)-i)
+    
--- Python/2025/4. Aprill 2025/Competitive programming/Practice/C++/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-   // for (int i=0; i<n-1; i++){
-    //     for(int j = i; j<i+1; j++){
-    //         cout << vec [j]<< " ";
-    //     }
-    // }
-    // cout << endl;
